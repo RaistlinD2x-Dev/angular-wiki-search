7f64be791eeac791088066319cc1b5b7faab47c4
Jesse <RaistlinD2x.dev@outlook.com>
2021-05-26 19:07:30 -0500

    setup event handler  for search bar

--- src/app/search-bar/search-bar.component.ts
+++ src/app/search-bar/search-bar.component.ts
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchBarComponent implements OnInit {
 
+  term = '';
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
 
 }
