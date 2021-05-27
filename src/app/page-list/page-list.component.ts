bae65901f960db8ca719f0f0a545984c3cb2b5ec
Jesse <RaistlinD2x.dev@outlook.com>
2021-05-26 23:20:18 -0500

    setup table for search response

--- src/app/page-list/page-list.component.ts
+++ src/app/page-list/page-list.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class PageListComponent implements OnInit {
 
-  @Input() pages = [];
+  @Input() pages: any[] = [];
 
   constructor() { }
 
